16831c4ad869726455f24d52448727583622452a
investorzhubz <investorzhubz@gmail.com>
2023-12-17 17:44:14 +0100

    intial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div  className="App">
       <Routes>
-        <Route path='/create' element={<Create />} />
+        <Route path='/' element={<Create />} />
         <Route path='/driving-record/licence-number' element={<GetLicense />} />
         <Route path='/driving-record/:id' element={<License />} />
       </Routes>
